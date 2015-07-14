2de91fecf7ba5891b8f6f221272050aa4dd8db64
BownX <xubowen92@gmail.com>
2015-07-15 00:24:35 +0800

    add comment

--- c/reverse_integer.c
+++ c/reverse_integer.c
@@ -1,3 +1,17 @@
+/*
+Reverse digits of an integer.
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+https://leetcode.com/problems/reverse-integer/
+*/
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdbool.h"
@@ -7,7 +21,7 @@ int reverse(int x) {
 	int MIN = -(int)((~(unsigned)0) >> 1) - 1;
 	int result = 0;
 	while(x != 0) {
-		// 在值*10之前预先一步判断是否overflow
+		// 在值*10之前预先一步判断是否overflow (虽然ac了但是严格来说判断不严谨，后面atoi那题更严谨一些)
 		if ((result > 0 && result > MAX / 10) || (result < 0 && result < MIN / 10)) {
 			// overflow
 			return 0;
